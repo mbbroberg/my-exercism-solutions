e3de70646e316793e9654f2338cba5ffb9aa668b
Matthew Broberg <1744971+mbbroberg@users.noreply.github.com>
2019-10-20 10:01:41 -0500

    with try wrapper

--- perfect_numbers.py
+++ perfect_numbers.py
@@ -1,23 +1,27 @@
 def classify(number):
-    aliquot_sum = 0
-    aliquot_series = []
+    try:
+        aliquot_sum = 0
+        aliquot_series = []
 
-    if number < 1:
-        raise ValueError
+        if number < 1:
+            raise ValueError
 
-    for n in range(1, number):
-        if number % n == 0:
-            aliquot_series.append(n)
-            aliquot_sum += n
-
-    if aliquot_sum == number:
-        return "perfect"
-    elif aliquot_sum > number:
-        return "abundant"
-    else:
-        return "deficient"
-    # print(f'sum is {aliquot_sum} with series {aliquot_series}')
-
+        for n in range(1, number):
+            if number % n == 0:
+                aliquot_series.append(n)
+                aliquot_sum+=n
+        
+        if aliquot_sum == number:
+            return "perfect"
+        elif aliquot_sum > number:
+            return "abundant"
+        else:
+            return "deficient"
+        # print(f'sum is {aliquot_sum} with series {aliquot_series}')
+    except ValueError:
+        print(f'An unexpected error occurred due to the value. Make sure number is greater than 0. Input was {number}')
+    except:
+        print("Something errored. Sorry about that. Please try again.")
 
 classify(-1)
 classify(10)
